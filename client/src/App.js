c406fc64dc6767f4125b14af171da5205d7a3e5d
Kubastrzt <kubastrzt@gmail.com>
2023-10-17 20:03:52 +0200

    Updated root page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+import React, {useEffect} from 'react';
+import {useStorePopup} from "./hooks/use-store-popup";
+
+const App = ()=>{
+    const onOpen = useStorePopup((state)=>state.onOpen);
+    const isOpen = useStorePopup((state)=>state.isOpen);
+
+    useEffect(() => {
+        if(!isOpen){
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
+    return(
+        <div>
+            Root
+        </div>
+    );
 }
 
-export default App;
+export default App
