ecbe5e55aa8a00b497e9d0798ec689f09f180239
Kubastrzt <kubastrzt@gmail.com>
2023-10-17 20:03:09 +0200

    Added reusable popup component with functionalities

---
+++ client/src/components/popups/store-popup.jsx
@@ -0,0 +1,14 @@
+import Popup from "../ui/popup";
+import {useStorePopup} from "../../hooks/use-store-popup";
+
+const StorePopup = ()=>{
+    const storePopup = useStorePopup();
+
+    return(
+        <Popup title='Create store' onClose={()=>storePopup.onClose()} isOpen={storePopup.isOpen} description='Add a new store to manage products'>
+            Future Create Store Form
+        </Popup>
+    )
+}
+
+export default StorePopup
