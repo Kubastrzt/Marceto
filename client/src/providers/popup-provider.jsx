22d44d6686abab3088d3900acfc04ab25fd3b5e1
Kubastrzt <kubastrzt@gmail.com>
2023-10-17 20:03:23 +0200

    Created provider for popup

---
+++ client/src/providers/popup-provider.jsx
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+
+import StorePopup from "../components/popups/store-popup";
+
+const PopupProvider = ()=>{
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, []);
+
+    if(!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            <StorePopup/>
+        </>
+    )
+}
+
+export default PopupProvider
