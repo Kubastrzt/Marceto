eb9458647c593bf9c26cba1f3a50922eac11f964
Kubastrzt <kubastrzt@gmail.com>
2023-10-28 15:10:44 +0200

    Consistency update

---
+++ client/src/components/StorePopup/StorePopup.jsx
@@ -0,0 +1,84 @@
+import Popup from "@/components/ui/popup";
+import {Input} from "@/components/ui/input";
+import {Form, FormField, FormItem, FormLabel, FormControl, FormMessage} from "@/components/ui/form";
+import {useStorePopup} from "@/hooks/useStorePopup";
+import {Button} from "@/components/ui/button";
+
+import * as zod from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import React, {useState} from "react";
+import axios from "axios";
+import {useAuth} from "@clerk/clerk-react";
+import {toast} from "react-toastify";
+
+const formSchema = zod.object({
+    name: zod.string().min(3),
+
+})
+
+const StorePopup = ()=>{
+    const storePopup = useStorePopup();
+    const {userId} = useAuth();
+
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    })
+
+    const onSubmit = async (values)=>{
+        setLoading(true);
+        const newValues = {...values, userId}
+
+        try {
+            await axios.post('http://localhost:3001/api/stores', newValues);
+
+            toast.success("Store has been created!", {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        } catch (err) {
+            toast.error("Something went wrong!", {
+                position: toast.POSITION.TOP_RIGHT
+            });
+
+            console.log(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return(
+        <>
+        <Popup title='Create store' onClose={()=>storePopup.onClose()} isOpen={storePopup.isOpen} description='Add a new store to manage products'>
+            <div className='space-y-4 py-2 pb-4'>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField controll={form.control}
+                        name='name'
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input disabled={loading} placeholder={"Shop name"} {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                        />
+                        <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+                            <Button disabled={loading} variant="outline" onClick={storePopup.onClose}>Cancel</Button>
+                            <Button disabled={loading} type="submit">Continue</Button>
+                        </div>
+                    </form>
+                </Form>
+            </div>
+        </Popup>
+        </>
+    )
+}
+
+export default StorePopup
