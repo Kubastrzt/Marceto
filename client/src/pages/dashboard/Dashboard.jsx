8e1a36ac3d22f070c708b3f9760e62d9fb2421ac
Kubastrzt <kubastrzt@gmail.com>
2023-11-02 19:40:02 +0100

    Created dashboard

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,1 +1,39 @@
-// keep
+import React, {useEffect, useState} from 'react';
+import {useAuth, UserButton} from "@clerk/clerk-react";
+import axios from "axios";
+import {useNavigate, useParams} from "react-router-dom";
+import Navigation from "@/components/Navigation/Navigation";
+
+const Dashboard = ()=>{
+    const [store, setStore] = useState({})
+    const {userId} = useAuth()
+    const params = useParams();
+    const navigate = useNavigate();
+
+    if(!userId) {
+        navigate('/sign-in/');
+    }
+
+    const getAvailableStore = async ()=>{
+        const response = await axios.get(`http://localhost:3001/api/user/${userId}/${params.sid}`)
+        setStore(response.data)
+    }
+
+    useEffect(() => {
+        getAvailableStore()
+    }, []);
+
+    if(!store) {
+        navigate('/');
+    }
+
+    return(
+        <>
+            <Navigation/>
+            Active store: {store?.name}
+            <UserButton/>
+        </>
+    );
+}
+
+export default Dashboard
