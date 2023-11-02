8a8b0ee5fa453fe52a684be17cd1efd593711c68
Kubastrzt <kubastrzt@gmail.com>
2023-11-02 19:38:40 +0100

    Modify provider scope

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/globals.css';
+import Dashboard from '@/pages/dashboard/Dashboard';
+import {useStorePopup} from "@/hooks/useStorePopup";
+import PopupProvider from "@/providers/PopupProvider";
 import {BrowserRouter, useNavigate, Route, Routes} from "react-router-dom";
 import {
     ClerkProvider, RedirectToSignIn, SignedIn, SignedOut,
     SignIn,
     SignUp
 } from "@clerk/clerk-react";
+import 'react-toastify/dist/ReactToastify.css';
 import {ToastContainer} from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -21,6 +25,15 @@ if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
 const ClerkWithRoutes = () =>{
     const navigate = useNavigate();
 
+    const onOpen = useStorePopup((state)=>state.onOpen);
+    const isOpen = useStorePopup((state)=>state.isOpen);
+
+    useEffect(() => {
+        if(!isOpen){
+            onOpen();
+        }
+    }, [isOpen, onOpen]);
+
     return (
         <ClerkProvider publishableKey={clerkPubKey} navigate={(to)=> {navigate(to)}}>
             <Routes>
@@ -28,15 +41,17 @@ const ClerkWithRoutes = () =>{
                     <>
                         <SignedIn>
                             <App/>
-                            <ToastContainer />
+                            <PopupProvider/>
+                            <ToastContainer/>
                         </SignedIn>
                         <SignedOut>
                             <RedirectToSignIn/>
                         </SignedOut>
                     </>
                 }/>
-                <Route path='/sign-up/*' element={<SignUp redirectUrl='/home' routing='path' path='/sign-up'/>}/>
-                <Route path='/sign-in/*' element={<SignIn redirectUrl='/home' routing='path' path='/sign-in'/>}/>
+                <Route path='/sign-up/*' element={<SignUp routing='path' path='/sign-up'/>}/>
+                <Route path='/sign-in/*' element={<SignIn routing='path' path='/sign-in'/>}/>
+                <Route path='/dashboard/:uid/:sid/' element={<Dashboard routing='path' path='/dashboard/:uid/:sid/'/>}/>
             </Routes>
         </ClerkProvider>
     )
